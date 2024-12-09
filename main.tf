b227ed072f5d274ce45e4a97449bb7b8cf715ebe
songithub061186 <jersonix@gmail.com>
2024-12-09 10:13:24 +0800

    New Proj

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0e2c8caa4b6378d8c" # us-east-1
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
