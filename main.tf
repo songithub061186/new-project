3f2ff220d6ad1e755a33945210adbe81c2fb6df1
jerson <jersonix@gmail.com>
2024-12-09 11:02:37 +0800

    Newproj4

--- main.tf
+++ main.tf
@@ -1,11 +1,24 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default subnet in the default VPC
+data "aws_subnet" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Create an EC2 instance in the default subnet
 resource "aws_instance" "Instance-Proj" {
-  ami           = "ami-0e2c8caa4b6378d8c" # us-east-1
+  ami           = "ami-0e2c8caa4b6378d8"  # us-east-1
   instance_type = "t2.micro"
+  subnet_id     = data.aws_subnet.default.id  # Specify subnet ID
+
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
