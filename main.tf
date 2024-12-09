299aa497b5292721609162209d5b8a2045d8fcd6
jerson <jersonix@gmail.com>
2024-12-09 11:13:04 +0800

    Newproj5

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_subnet" "default" {
 
 # Create an EC2 instance in the default subnet
 resource "aws_instance" "Instance-Proj" {
-  ami           = "ami-0e2c8caa4b6378d8"  # us-east-1
+  ami           = "ami-0e2c8caa4b6378d8c"  # us-east-1
   instance_type = "t2.micro"
   subnet_id     = data.aws_subnet.default.id  # Specify subnet ID
 
